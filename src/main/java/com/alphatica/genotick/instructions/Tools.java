3f5719dadd346ddafa2a4f0702387fb3cffab58b
Łukasz Wojtów <lukasz.wojtow@gmail.com>
2018-02-14 22:46:33 +0100

    Fix double mutation

--- src/main/java/com/alphatica/genotick/instructions/Tools.java
+++ src/main/java/com/alphatica/genotick/instructions/Tools.java
@@ -4,11 +4,14 @@ import com.alphatica.genotick.mutator.Mutator;
 
 class Tools {
     static double mutateDouble(double argument, Mutator mutator) {
-        double next = argument * mutator.getNextDouble();
-        if(argument > 0) {
-             return argument + next;
-        } else {
-            return argument - next;
-        }
+        return argument == 0 ? limitedRandom(1000, mutator) : mutateArgument(argument, mutator);
+    }
+
+    private static double mutateArgument(double argument, Mutator mutator) {
+        return argument * limitedRandom(20, mutator);
+    }
+
+    private static double limitedRandom(double limit, Mutator mutator) {
+        return limit * mutator.getNextDouble();
     }
 }
